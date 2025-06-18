a12ff3ef25be7d7b907c8fb5567fdeb3b838a421
duyenduyenduyen318 <duyenduyenduyen318@gmail.com>
2025-06-18 04:43:14 +0000

    first commit

---
+++ src/bootstrap.js
@@ -0,0 +1,12 @@
+window["APP_TEMPLATE_BOOTSTRAP"] = {
+    geminiApiKey: "AIzaSyA01VXt-NTTgDSzcDASWNWMZwlVKUNa_0Y",
+
+    firebase: {
+      apiKey: "AIzaSyBoqDcxKcrjpjrCSYkN4OIA5Gvmhk4JhRo",
+      authDomain: "shinhan-ekyc.firebaseapp.com",
+      projectId: "shinhan-ekyc",
+      storageBucket: "shinhan-ekyc.firebasestorage.app",
+      messagingSenderId: "65262042526",
+      appId: "1:65262042526:web:797c8831e691c9ae7a6a8f",
+    }
+    };
