f5953c16311ea58ea1280716fc9ffc540034f10d
shinhan Finance <thuytrangely318@gmail.com>
2025-06-29 18:06:29 +0700

    Add files via upload

---
+++ configs/web/config.js
@@ -0,0 +1,18 @@
+// public/js/config.js
+
+// Tệp này chứa các thông tin xác thực của bạn từ VNPT.
+// Bạn chỉ cần chỉnh sửa tệp này khi cần cập nhật token.
+export const apiConfig = {
+    // Thay đổi này thành tên miền API của bạn nếu cần
+    VNPT_DOMAIN: 'https://api.idg.vnpt.vn',
+    
+    // Token truy cập dịch vụ
+    ACCESS_TOKEN: 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0cmFuc2FjdGlvbl9pZCI6ImZkZTcyZGNmLTA5ZGQtNDcxNC1iOWY0LTU0ZmU4ODEyMDhhZSIsInN1YiI6ImRjNjNmZDFiLTRlM2MtMTFmMC1hNThlLTVmYmE5NDZmYmUzMCIsImF1ZCI6WyJyZXN0c2VydmljZSJdLCJ1c2VyX25hbWUiOiJ0aHV5dHJhbmdlbHkzMThAZ21haWwuY29tIiwic2NvcGUiOlsicmVhZCJdLCJpc3MiOiJodHRwczovL2xvY2FsaG9zdCIsIm5hbWUiOiJ0aHV5dHJhbmdlbHkzMThAZ21haWwuY29tIiwiZXhwIjoxNzUwNTU0Nzc1LCJ1dWlkX2FjY291bnQiOiJkYzYzZmQxYi00ZTNjLTExZjAtYTU4ZS01ZmJhOTQ2ZmJlMzAiLCJhdXRob3JpdGllcyI6WyJVU0VSIl0sImp0aSI6IjA2ZmU4Y2IzLWU2YmUtNGYxNS04MDIwLTRlOGI1Mjg2ZWVhZCIsImNsaWVudF9pZCI6ImNsaWVudGFwcCJ9.OOZH5JsGIr52Y_W4QpX0ksP1tYr1GqzAU55rsiSuLFTFuM3xqxTWaoKq3innWpbaOYaM4PIjXUSDVMLy8ADlUjTjn8cqDQXv63ZjF56SDnK7IN3ChjD20vx_j3QJ3IbvsFHFedEz1DpfB46E1lYr_bimi1DKWjeKhFkf9an7UbRtVNfUEuLJDqnmzuG6FRgcdeZHVdxqlJoSwG9rjprzYJrDU3vsRASsv0D9kMWKgvvx75O3UO-TYje6ul8nyOqhft3w7yd6rOCcaVxOf6xfJHhD-mpqLK6zi-hldtBYfHAcI-XJavVLWQmTnIg3lPxCMAS4_1Vma285ixE0EPUL5w',
+    
+    // Token ID
+    TOKEN_ID: '380badb6-f30a-5913-e063-63199f0a8a5f',
+    
+    // Token Key
+    TOKEN_KEY: 'MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBALSe2qB4QW5JdFSWv3OYstD3HfsGhW0QhG8hVDwc/3cyPyuNVV4fWY34hkS0TapQKXtpavujrbnASe/XiJvNJwcCAwEAAQ==',
+};
+
